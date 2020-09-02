14e9e9fd14961aa32836dd9bdc9d95dc0280c0c0
Fynn <pacmac@n143-p134.eduroam.kth.se>
2020-09-02 16:47:32 +0200

    init

---
+++ HMM0/kth.ai.hmm0/hmm0.py
@@ -0,0 +1,35 @@
+"""
+Do:
+Matrix multiplications
+
+Given information:
+
+"""
+
+a = [[0.2, 0.5, 0.3, 0.0],
+     [0.1, 0.4, 0.4, 0.1],
+     [0.2, 0.0, 0.4, 0.4],
+     [0.2, 0.3, 0.0, 0.5]]
+
+b = [[1.0, 0.0, 0.0],
+     [0.0, 1.0, 0.0],
+     [0.0, 0.0, 1.0],
+     [0.2, 0.6, 0.2]]
+
+pi = [0.0, 0.0, 0.0, 1.0]
+
+
+def matrixMultiply(m1, m2):
+    #TODO check if matrices mult-able
+    return [[0 for x in range(3)] for y in range(3)]
+
+def main():
+    print("HMM_0")
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
