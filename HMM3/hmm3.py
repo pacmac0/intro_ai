66b22749f6e103cdff7d6b57627ab3a13fe09934
Fynn <pacmac@n136-p125.eduroam.kth.se>
2020-09-10 16:03:48 +0200

    update

--- HMM3/hmm3.py
+++ HMM3/hmm3.py
@@ -46,10 +46,10 @@ def getMatricesFromStdIn():
         else:
             pass
 
-def elemVectorMult(vec1, vec2):
+def elemVectorMult(vec1, vec2): # returns [[]]
     return [[vec1[e] * vec2[e] for e in range(len(vec1))]]
 
-def getObsColumnByIndex(idx):
+def getObsColumnByIndex(idx): # returns []
     if idx >= len(b[0]):
         sys.stderr.write("index out of range")
     return [row[idx] for row in b]
@@ -57,59 +57,70 @@ def getObsColumnByIndex(idx):
 """
 in case of vector multiplication add vector first!
 """
-def getViterbiDelta(prev_delta, ob):
-    res_vec = [[0 for col in range(len(prev_delta[0]))]]
-    obsCol = getObsColumnByIndex(ob)
+def matrixMultiply(mat1, mat2): # returns [[]]
+    res_mat = [[0 for row in range(len(mat2[0]))] for col in range(len(mat1))]
+    for row1_idx, row in enumerate(mat1):
+        for col2_idx in range(len(mat2[0])):
+            for val_idx in range(len(row)):
+                res_mat[row1_idx][col2_idx] += row[val_idx] * mat2[val_idx][col2_idx]
+                res_mat[row1_idx][col2_idx] = float("{:.8f}".format(res_mat[row1_idx][col2_idx])) # rounding to two digits
+    return res_mat
 
-    for col in range(len(a[0])):
-        max = 0
-        for row in range(len(a)):
-            val = a[row][col] * prev_delta[0][row] * obsCol[col]
-            #print("{} * {} * {} = {}".format(prev_delta[0][row], a[row][col], obsCol[col], val))
-            if val > max:
-                max = val
-        res_vec[0][col] = float("{:.8f}".format(max)) # rounding to two digits
-    return res_vec
+def matrixTranspose(m):
+    return list(map(list, zip(*m)))
+
+def getNextAlpha(ob_idx): # returns [[]]
+    return elemVectorMult(matrixMultiply(alpha, a)[0], getObsColumnByIndex(ob_idx))
+
+def getNextBeta(ob_idx): # returns [[]]
+    beta[0] = matrixMultiply(a, matrixTranspose(elemVectorMult(getObsColumnByIndex(ob_idx), beta[0])))
+    return matrixTranspose(beta[0]) # transpose back
+
+def getDi_Gamma():
+
+    return
+
+def getGamma():
+    return
 
 def main():
     """
     getMatricesFromStdIn()
     global obs
     obs = [int(ob) for ob in obs[0]]
+
     """
     global a
     a = [[0.6, 0.1, 0.1, 0.2], 
          [0.0, 0.3, 0.2, 0.5], 
          [0.8, 0.1, 0.0, 0.1],
          [0.2, 0.0, 0.1, 0.7]]
-
     global b
-    b = [[0.6, 0.2, 0.1, 0.1],
+    b= [[0.6, 0.2, 0.1, 0.1],
         [0.1, 0.4, 0.1, 0.4],
         [0.0, 0.0, 0.7, 0.3],
         [0.0, 0.0, 0.1, 0.9]]
-
     global pi
     pi = [[0.5, 0.0, 0.0, 0.5]]
-
     global obs
-    obs = [2, 0, 3, 1]
+    obs = [3, 0, 0, 2]
     
-    delta_timeline = []
-    prev_delta = elemVectorMult(pi[0], getObsColumnByIndex(obs[0]))
-    delta_timeline.append(prev_delta)
+    global di_gamma
+    di_gamma = [[0 for n in pi[0]]]
+    global gamma
+    gamma = [[0 for n in pi[0]]]
     
+    global alpha
+    alpha = elemVectorMult(pi[0], getObsColumnByIndex(obs[0])) # inits alpha by element vise multiplication of pi and first observations emission prob.
     for ob in obs[1:]:
-        prev_delta = getViterbiDelta(prev_delta, ob)
-        delta_timeline.append(prev_delta)
+        alpha = getNextAlpha(ob)
     
-    print(delta_timeline)
-    states = [max(enumerate(delta), key=lambda x: x[1])[0] for delta in delta_timeline]
-
-    sys.stdout.write(str(states).strip(' []').replace(',', '') + '\n')
-
-
+    global beta
+    beta = [[1 for x in pi[0]]]
+    for ob in obs[::-1]:
+        beta = getNextBeta(ob)
     
+
 
 if __name__ == "__main__":
     main()
